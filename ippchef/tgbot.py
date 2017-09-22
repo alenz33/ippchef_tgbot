b0a424e0586ec2a8e5a48551e9b673aac9ba5887
Alexander Lenz <alexander.lenz@frm2.tum.de>
2017-09-22 16:19:55 +0200

    Add subscription prototype.

--- ippchef/tgbot.py
+++ ippchef/tgbot.py
@@ -20,8 +20,12 @@
 #
 # *****************************************************************************
 
-import datetime
+import time
+import re
 import functools
+import threading
+
+from datetime import date, datetime, timedelta, time as dttime
 
 from telegram.ext import Updater, CommandHandler
 from telegram import KeyboardButton, ReplyKeyboardMarkup
@@ -41,19 +45,62 @@ Inquiries about the bot at @kuryfox.
 '''
 
 
+class NotificationLoop(threading.Thread):
+    def __init__(self, log, bot, loop_delay=15):
+        threading.Thread.__init__(self)
+        self.log = log.getChild('notifier')
+        self._bot = bot
+        self._loop_delay = loop_delay
+        self._sub_chats = {}
+
+    @property
+    def subscriptions(self):
+        return self._sub_chats
+
+    def subscribe_chat(self, chat, timeobj):
+        self.unsubscribe_chat(chat)
+
+        self._sub_chats[chat] = (timeobj, None)
+
+    def unsubscribe_chat(self, chat):
+        if chat in self._sub_chats:
+            del self._sub_chats[chat]
+
+    def run(self):
+        self.log.info('Start notification loop')
+        while True:
+            try:
+                today = date.today()
+                now = datetime.now().time()
+                for chat, (ntime, last) in self._sub_chats.items():
+                    if (not last or last < today) and ntime < now:
+                        self._notify(chat)
+                        self._sub_chats[chat] = (ntime, today)
+            except Exception as e:
+                self.log.exception(e)
+            time.sleep(self._loop_delay)
+
+    def _notify(self, chat):
+        self.log.debug('Send scheduled notification to: %s ...' % chat)
+        self._bot._update_cache()
+        self._bot._send(chat, self._bot._cache[1]['today'])
+
+
 class Bot(object):
     def __init__(self, log, api_key, jid, jpw, djid):
         self.log = log
-        self._sub_times = {}
         self._sub_chats = {}
         self._cache = (None, {})
         self._last_error = None
 
+        self._updater = Updater(api_key)
+        self._bot = self._updater.bot
+
         self._xmpp = XMPPConnection(log, jid, jpw, djid)
         self._xmpp.start()
 
-        self._updater = Updater(api_key)
-        self._bot = self._updater.bot
+        self._notifier = NotificationLoop(self.log, self)
+        self._notifier.start()
 
         self._create_guarded_cmd('start', self.cmd_start)
         self._create_guarded_cmd('today', self.cmd_today)
@@ -87,17 +134,20 @@ class Bot(object):
         self._reply(update, self._cache[1]['tomorrow'])
 
     def cmd_subscribe(self, bot, update):
-        msg = update.message['text'][10:].strip()
+        msg = re.sub(r'/subscribe(@ippchef_bot)?', '',
+                     update.message['text']).strip()
 
-        print(update.effective_chat)
+        if not re.match('\d{1,2}:\d\d', msg):
+            raise ValueError('Invalid time format. Please use hh:mm')
 
-        raise NotImplementedError('Command not implemented yet')
+        subtime = dttime(*map(int, msg.split(':')))
+        self._notifier.subscribe_chat(update.effective_chat['id'], subtime)
+
+        self._reply(update, 'Subscription successful!')
 
     def cmd_unsubscribe(self, bot, update):
-
-        uid = update.effective_user['id']
-
-        raise NotImplementedError('Command not implemented yet')
+        self._notifier._unsubscribe_chat(update.effective_chat['id'])
+        self._reply(update, 'Unsubscription successful!')
 
     def cmd_debug(self, bot, update):
         result = ['<b>DEBUG INFO</b>', '']
@@ -109,6 +159,14 @@ class Bot(object):
             dt, cmd, user, error = self._last_error
             result.append('%s:\n<i>%s</i> for <i>%s</i>:\n<pre>%s</pre>'
                           % (dt.ctime(), cmd, user, error))
+
+        result.append('')
+        result.append('<b>Subscriptions:</b>')
+
+        for chat, (subtime, last) in self._notifier.subscriptions.items():
+            result.append('  - %s: %s (last: %s)' % (chat,
+                                                     subtime.isoformat(),
+                                                     last))
 
         self._reply(update, '\n'.join(result))
 
@@ -125,7 +183,7 @@ class Bot(object):
         return '\n'.join(menu)
 
     def _update_cache(self):
-        today = datetime.date.today()
+        today = date.today()
         if self._cache[0] != today:
             self.log.debug('Cache needs to be updated ...')
             self._cache = (today, {
@@ -133,7 +191,7 @@ class Bot(object):
                             self._xmpp.communicate('ipp heute'), today),
                 'tomorrow': self._reformat_menu(
                     self._xmpp.communicate('ipp morgen'),
-                    today + datetime.timedelta(days=1))
+                    today + timedelta(days=1))
             })
 
     def _reply(self, update, reply, **kwargs):
@@ -155,7 +213,7 @@ class Bot(object):
             return func(bot, update)
         except Exception as e:
             self.log.exception(e)
-            self._last_error = (datetime.datetime.now(), cmd, user, e)
-            self._reply(update, 'Error during command execution, please take a '
-                                'look at the log files for further '
-                                'information.')
+            self._last_error = (datetime.now(), cmd, user, e)
+            self._reply(update, 'Error during command execution:\n\n<pre>%s'
+                                '</pre>\n\nPlease take a look at the log '
+                                'files for further information.' % e)
